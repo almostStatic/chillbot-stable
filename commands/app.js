838e1c20a14ac96afcc86c34dc143c3c5266f05d
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-04-26 18:27:32 +0100

    Create app.js

---
+++ commands/app.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+
+
+module.exports.run = (client, message, args, level) => {
+
+    if(message.author.id !== "501710994293129216") return message.channel.send("This is am owner-only command!");
+
+    let appEmbed = new Discord.RichEmbed()
+
+    .setTitle("Link to app hosted on Heroku")
+    .setURL("https://dashboard.heroku.com/apps/bot-hosted-using-heroku/deploy/github")
+
+    message.author.send({embed: appEmbed});
+        
+}
+
+module.exports.help = {
+    name: "app"
+  }
+  
