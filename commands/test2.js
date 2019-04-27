0d8ed99867a2049c272f870b2ef8fcca7a84134b
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-04-27 08:43:48 +0100

    updated commands

---
+++ commands/test2.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+    let onlyinnsfwchannel = new Discord.RichEmbed()
+
+    .setDescription("You can only use this commmand in an nsfw channel!")
+    .setColor("RANDOM")
+
+    if(message.channel.type !== "nsfw") return message.channel.send({embed: onlyinnsfwchannel});
+
+    // If it is an NSFW Channel:
+
+    message.channel.send("Works!!");
+
+}
+
+module.exports.help = {
+  name: "test2"
+}
