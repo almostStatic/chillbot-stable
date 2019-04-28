ecaa07e45568873c1d96bcde773a0347b59622ef
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-04-28 13:27:37 +0100

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
         // This command removes all messages from all users in the channel, up to 100.
         
         // get the delete count, as an actual number.
-        const deleteCount = parseInt(args[0], 10);
+        const deleteCount = parseInt(args[0], 10)+ 1;
         
         // Ooooh nice, combined conditions. <3
         if(!deleteCount || deleteCount < 1 || deleteCount > 100)
