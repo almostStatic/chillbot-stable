cf7497a468f034cd15184d5972ef72682487d1c3
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-04-29 09:11:31 +0100

    Update guilds.js

--- commands/guilds.js
+++ commands/guilds.js
@@ -2,14 +2,15 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    let botguilds = bot.guilds
     let embed = new Discord.RichEmbed()
 
-    .setDescription("\n \n", bot.guilds)
+    .setDescription(`${botguilds}`)
     .setTitle("Here is a list of guilds that the bot is in:")
     .setColor("#398473")
 
     message.channel.send(embed);
-    
+
 
 }
 
