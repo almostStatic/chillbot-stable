9bb227f0a25d67de8cdaca5de07ee6d957021e58
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-07 10:35:05 +0100

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -2,16 +2,18 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    
+    if(message.author.id !== "") return message.reply("This is an owner-only command!")
     let tolog = args.join(" ");
     const logEmbed = new Discord.RichEmbed()
 
-    .setTitle(`${message.member.user.tag} has logged:`)
+
     .setDescription(tolog)
     .setColor("RANDOM")
     .setImage(message.author.avatarURL)
     .setTimestamp()
 
-    let logChannel = message.guild.channels.find(`name`, "logs");
+    let logChannel = message.guild.channels.find(`name`, "bot-logs");
     if(!logChannel) return message.channel.send("Can't find logs channel!");
     message.channel.send("Logged!")
 
