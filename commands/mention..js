b8962400d77cbb8c2e8473a4757d8253b5e665cb
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-07 17:48:15 +0100

    Create mention..js

---
+++ commands/mention..js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+
+module.exports.help = async (bot, message, args) => {
+    let toping = args.join(" ");
+
+    message.channel.send(`${toping}, you have been summoned!`)
+
+    if(!toping) return message.reply("Who should I ping?")
+}
+
+
+module.exports.help = {
+    name: "mention"
+}
