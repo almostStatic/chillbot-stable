26f290135001572fadb51ae97c58e7a4a30edb65
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-07 18:01:16 +0100

    Update mention..js

--- commands/mention..js
+++ commands/mention..js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 
-module.exports.help = async (bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
     let toping = args.join(" ");
 
     message.channel.send(`${toping}, you have been summoned!`)
