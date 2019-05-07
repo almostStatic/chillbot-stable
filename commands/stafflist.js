caf472c5c4af8cbddc9ab37c29a184a04a3fbb18
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-07 18:27:03 +0100

    Update stafflist.js

--- commands/stafflist.js
+++ commands/stafflist.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let tosend = new Discord.RichEmbed()
 
     .setTitle("Staff List:")
-    .setDescription("\n <@425165710847770634> [Admin] \n <@336920581624692737> [Admin] [Trusted] \n <@163715276733415426> [Admin] [Trusted] \n <@366692921245958146> [Moderator] [Trusted] \n <@293841631583535106> [Admin] [Trusted] \n <@429982720257687572> [Admin] \n <@171397553160585217> [Admin] [Trusted] \n <@373900508026372097> [Co-Owner] [Head Admin] [Very Trusted] \n <@452569370825326593> [Admin]")
+    .setDescription("\n <@425165710847770634> [Admin] \n <@336920581624692737> [Admin] [Trusted] \n <@163715276733415426> [Admin] [Trusted] \n <@366692921245958146> [Moderator] [Trusted] \n <@293841631583535106> [Admin] [Trusted] \n <@429982720257687572> [Admin] \n <@171397553160585217> [Admin] [Trusted] \n <@373900508026372097> [Co-Owner] [Head Admin] [Very Trusted]")
     .setColor("RANDOM")
 
     message.channel.send({embed: tosend});
