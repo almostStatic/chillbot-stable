f76776470a4b0a291d827f546a9b4644b24451c4
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 14:41:46 +0100

    Update tsay.js

--- commands/tsay.js
+++ commands/tsay.js
@@ -8,6 +8,14 @@ module.exports.run = async (bot, message, args) => {
       if(!sayMessage) return message.reply("Make sure you tell me what you want me to say...")
       message.delete().catch();
       message.channel.send(sayMessage);
+   
+   const used = new Discord.RichEmbed()
+
+.setTitle("Command Used:")
+.setDescription(`c.tsay used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+.setColor("RANDOM")
+bot.channels.get("575619138576318484").send(used);
+
 
   // , "431684183371415555"
 
