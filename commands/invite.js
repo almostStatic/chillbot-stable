c6b3ea5bac36da244e8c8f2ae1fff81368288110
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 12:12:19 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,6 +3,14 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     message.channel.send(`**Invite people to this server using:** \n https://discord.gg/YUjuY6E`)
+const used = new Discord.RichEmbed()
+
+.setTitle("Command Used:")
+.setDescription(`c.invite used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+.setColor("RANDOM")
+bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
