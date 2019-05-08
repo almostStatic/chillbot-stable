d1aaede491e8c9b68ea0e86401d99d56c1488ab9
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 12:00:05 +0100

    Update hosting.js

--- commands/hosting.js
+++ commands/hosting.js
@@ -10,6 +10,15 @@ module.exports.run = async (bot, message, args) => {
     .setTimestamp()
   
      message.channel.send(hostembed);
+    
+        const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.hosting used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
