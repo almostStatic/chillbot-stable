54d657f5248696476e9f69cd2ae87f8ec95204b6
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 11:56:11 +0100

    Update drink.js

--- commands/drink.js
+++ commands/drink.js
@@ -8,6 +8,14 @@ module.exports.run = async (bot, message, args) => {
     .setColor('RANDOM')
 
     message.channel.send(drinkEmbed);
+        const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.drink used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
