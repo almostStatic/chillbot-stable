c11ccd286da876897e6e352e046a35eb48648f8b
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 11:52:19 +0100

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -18,6 +18,15 @@ module.exports.run = async (bot, message, args) => {
   .addField("💸", uCoins);
 
   message.channel.send(coinEmbed);
+  
+      const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.coins used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
