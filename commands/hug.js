ff6fdf86c3dae53914e5d8b70a95a8359a418323
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 12:00:47 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -10,6 +10,15 @@ if(message.channel.type === "dm") return;
     .setDescription(`${message.author.tag} has hugged ${member.user.tag}`)
     .setColor('RANDOM')
     message.channel.send(hugEmbed)
+      
+          const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.hug used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id}), to hug ${member.user.tag}`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
     
 
