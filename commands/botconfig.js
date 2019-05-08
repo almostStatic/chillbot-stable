5c8643f2a825fc7aa64aa1fece5c97013b3ef147
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 11:49:08 +0100

    Update botconfig.js

--- commands/botconfig.js
+++ commands/botconfig.js
@@ -5,13 +5,21 @@ module.exports.run = async (bot, message, args) => {
 const embed = new Discord.RichEmbed()
 
     .setTitle("Bot Configuratoin and stuffs")
-    .addField("Prefix: `/`", " UNCHANGEABE | THIS WILL REMAIN THE SAME")
+    .addField("Prefix: `c.`", " UNCHANGEABE | THIS WILL REMAIN THE SAME")
     .addField("**Running On**", "main.js")
     .addField("Command Handler Status", "In use | Fully Working")
     .setColor("RANDOM")
     .setFooter("a footer", message.author.avatarURL)
 
         message.channel.send(embed);
+        const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.botconfig used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
