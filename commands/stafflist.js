a22b6ae9540f308b7af385b8d7589ce0d6749212
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 14:35:05 +0100

    Update stafflist.js

--- commands/stafflist.js
+++ commands/stafflist.js
@@ -8,6 +8,14 @@ module.exports.run = async (bot, message, args) => {
     .setColor("RANDOM")
 
     message.channel.send({embed: tosend});
+    
+    const used = new Discord.RichEmbed()
+
+        .setTitle("Command Used:")
+        .setDescription(`c.staff-list used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+        .setColor("RANDOM")
+        bot.channels.get("575619138576318484").send(used);
+
 }
 
 module.exports.help = {
