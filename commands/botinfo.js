d992c5388d14d49963aa3e96bf519926127d060b
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 11:49:33 +0100

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -23,6 +23,14 @@ module.exports.run = async (bot, message, args) => {
     .setTimestamp()
 
     return message.channel.send(botembed);
+      const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.botinfo used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
