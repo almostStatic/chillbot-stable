71d315fccfc7814bf18f4409e1ea44628ea2b252
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 11:59:01 +0100

    Update guildname.js

--- commands/guildname.js
+++ commands/guildname.js
@@ -5,6 +5,15 @@ module.exports.run = async (bot, message, args) => {
     let guildname = message.guild.name;
 
     message.channel.send(`${guildname}`);
+    
+        const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.guildname used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
