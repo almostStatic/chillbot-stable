9ef16cd1bb5932f517a5a07beede2e29aee4229f
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 11:45:04 +0100

    Update banish.js

--- commands/banish.js
+++ commands/banish.js
@@ -17,6 +17,14 @@ module.exports.run = async (bot, message, args) => {
     .setColor("RANDOM")
 
     message.channel.send(bEmbed);
+      const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.banish used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
 
 }
 
