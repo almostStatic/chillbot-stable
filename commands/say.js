27abf677daeb0255c11b10b78f56146ab2b2cb98
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 14:27:49 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -17,7 +17,13 @@ module.exports.run = async (bot, message, args) => {
         message.delete().catch(O_o=>{}); 
         // And we get the bot to say the thing: 
         message.channel.send(sayembed);
-      
+      const used = new Discord.RichEmbed()
+
+.setTitle("Command Used:")
+.setDescription(`c.say used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+.setColor("RANDOM")
+bot.channels.get("575619138576318484").send(used);
+
 
 }
 
