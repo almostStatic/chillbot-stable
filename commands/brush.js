c474de61104c266d01f61307c8ad70014e0aec16
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 11:50:02 +0100

    Update brush.js

--- commands/brush.js
+++ commands/brush.js
@@ -8,6 +8,14 @@ module.exports.run = async (bot, message, args) => {
     .setColor(white);
     
     message.channel.send(brushEmbed);
+        const used = new Discord.RichEmbed()
+
+    .setTitle("Command Used:")
+    .setDescription(`c.brush used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+    bot.channels.get("575619138576318484").send(used);
+
+
     
 
 }
