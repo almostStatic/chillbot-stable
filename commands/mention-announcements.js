54012ecc8e7b8db1fa3c24cd6fc443a9b72a528f
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 12:37:39 +0100

    Update mention-announcements.js

--- commands/mention-announcements.js
+++ commands/mention-announcements.js
@@ -4,6 +4,13 @@ module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("MENTION_EVERYONE")) return message.reply(":face_pain: Nope. :face_pain:")   
     message.channel.send(`<@${message.author.id}> has mentioned <@&573084849532043274>!`)
+    
+        const used = new Discord.RichEmbed()
+
+.setTitle("Command Used:")
+.setDescription(`c.ma used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+.setColor("RANDOM")
+bot.channels.get("575619138576318484").send(used);
 }
 
 module.exports.help = {
