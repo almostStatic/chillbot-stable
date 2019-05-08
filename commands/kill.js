fb0a67564197ae9e41dbfab8e6ef7ec46e9be7b0
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 12:21:30 +0100

    fixed

--- commands/kill.js
+++ commands/kill.js
@@ -3,28 +3,17 @@ const client = new Discord.Client();
 
 module.exports.run = async (bot, message, args) => {
 
-    const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.kill used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
 
 
                 if(message.author.id !== "501710994293129216") return;
-const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.kill used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+                    bot.channels.get("575619138576318484").send(used);
 
 
 
-        message.channel.send("Goodbye!")
+                             message.channel.send("Goodbye!")
     
     
-        process.exit()
+                                          process.exit()
 
 
 }
