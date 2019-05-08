76e0287d01850adc4e5f110e3883a9d38ff4ab4d
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 12:42:18 +0100

    Update mention..js

--- commands/mention..js
+++ commands/mention..js
@@ -6,6 +6,13 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(`${toping}, you have been summoned!`)
 
     if(!toping) return message.reply("Who should I ping?")
+    
+        const used = new Discord.RichEmbed()
+
+.setTitle("Command Used:")
+.setDescription(`c.mention used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+.setColor("RANDOM")
+bot.channels.get("575619138576318484").send(used);
 }
 
 
