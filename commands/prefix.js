c78755a2569a9a87f9eb86e00d171730f5749daa
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 13:16:49 +0100

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -9,6 +9,13 @@ module.exports.run = async (bot, message, args) => {
     .setTimestamp()
 
     message.channel.send(prefixEmbed);
+    
+            const used = new Discord.RichEmbed()
+    
+    .setTitle("Command Used:")
+    .setDescription(`c.pet <reason> used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+    .setColor("RANDOM")
+      bot.channels.get("575619138576318484").send(used);
 
 }
 
