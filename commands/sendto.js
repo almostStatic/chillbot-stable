621fa09f9715707c4b09085cdc13c12b11886858
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-08 14:31:09 +0100

    Update sendto.js

--- commands/sendto.js
+++ commands/sendto.js
@@ -9,6 +9,14 @@ module.exports.run = async (bot, message, args) => {
 // <#560553771353112577>
 
 // /send-to <#560553771353112577> test
+    
+    const used = new Discord.RichEmbed()
+
+.setTitle("Command Used:")
+.setDescription(`c.send-to used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
+.setColor("RANDOM")
+bot.channels.get("575619138576318484").send(used);
+
 }
 
 module.exports.help = {
