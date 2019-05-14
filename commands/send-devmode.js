4543a6d7efd712ef8e471d645387e18c94f5802b
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:46:01 +0100

    Update send-devmode.js

--- commands/send-devmode.js
+++ commands/send-devmode.js
@@ -20,12 +20,11 @@ module.exports.run = async (bot, message, args) => {
     sendto.send({embed: devembed});
     message.channel.send("Your message has been sent!")
     
-    const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.send-devmode used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.send-devmode used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 
 
   }
