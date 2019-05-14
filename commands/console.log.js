2d06de0308cb541e5bb541fdd91d2f8ee2297040
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 11:51:16 +0100

    Update console.log.js

--- commands/console.log.js
+++ commands/console.log.js
@@ -7,12 +7,11 @@ module.exports.run = async (bot, message, args) => {
 let tolog = args.join(" ");
 console.log(tolog);
 message.channel.send(`Attempted to log \`${tolog}\` on the console!`)
-        const used = new Discord.RichEmbed()
-
-    .setTitle("Command Used:")
-    .setDescription(`c.clog used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id}) to log ${tolog}`)
-    .setColor("RANDOM")
-    bot.channels.get("575619138576318484").send(used);
+let used = new Discord.RichEmbed()
+.setAuthor(`Command Used:`, bot.user.avatarURL)
+.setColor(`#81868e`)
+.setDescription(`c.clog used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+bot.channels.get("575619138576318484").send(used)
 
 
 
