1a1fd7e883a3a45b17d53db6baa6aafcf4c0d9e6
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 11:45:00 +0100

    Update app.js
    changed log message

--- commands/app.js
+++ commands/app.js
@@ -13,13 +13,12 @@ module.exports.run = (client, message, args, level) => {
     
     message.author.send({embed: appEmbed});
     message.channel.send("Message Sent!")
-        const used = new Discord.RichEmbed()
 
-    .setTitle("Command Used:")
-    .setDescription(`c.app used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-    .setColor("RANDOM")
-    bot.channels.get("575619138576318484").send(used);
-
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.app used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 
         
 }
