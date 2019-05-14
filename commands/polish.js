84629daa9c34afdb1318b763d41cc2dd95a643c7
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:45:10 +0100

    Update polish.js

--- commands/polish.js
+++ commands/polish.js
@@ -20,14 +20,12 @@ module.exports.run = async (bot, message, args) => {
       message.channel.send(embed);
       message.channel.send(".");
   
-    const used = new Discord.RichEmbed()
-    
-    .setTitle("Command Used:")
-    .setDescription(`c.pet <reason> used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-    .setColor("RANDOM")
-      bot.channels.get("575619138576318484").send(used);
+      let used = new Discord.RichEmbed()
+      .setAuthor(`Command Used:`, bot.user.avatarURL)
+      .setColor(`#81868e`)
+      .setDescription(`c.polish used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+      bot.channels.get("575619138576318484").send(used)
 }
-
 module.exports.help = {
-  name: "polish"
+  name: "polish",
 }
