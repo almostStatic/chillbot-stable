372ac9b9c02fd37b19166933dfb031e84ca98c86
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 11:51:20 +0100

    Update dev-mode.js

--- commands/dev-mode.js
+++ commands/dev-mode.js
@@ -16,12 +16,11 @@ module.exports.run = async (bot, message, args) => {
     .setFooter("Use /invite to invite me to your server!")
 
     message.channel.send({embed: devembed});
-        const used = new Discord.RichEmbed()
-
-    .setTitle("Command Used:")
-    .setDescription(`c.dev-mode used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-    .setColor("RANDOM")
-    bot.channels.get("575619138576318484").send(used);
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.dev-mode used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 
 
 
