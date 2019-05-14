7e4c6b03ee2e89efe622d9c792cf31824d8d3dad
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 13:54:43 +0100

    Update log.js

--- commands/log.js
+++ commands/log.js
@@ -18,13 +18,11 @@ module.exports.run = async (bot, message, args) => {
     reportschannel.send(logEmbed);
 
     message.channel.send("Logged!")
-        const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.log used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
-
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.log used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 }
 
 module.exports.help = {
