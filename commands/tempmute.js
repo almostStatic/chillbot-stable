3c564dbd4b0dd0f59fa68440b466ec81dab44085
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:46:37 +0100

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -49,13 +49,11 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(`<@${tomute.id}> has been unmuted!`);
   }, ms(mutetime));
   
-  const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.tempmute used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
-
+  let used = new Discord.RichEmbed()
+  .setAuthor(`Command Used:`, bot.user.avatarURL)
+  .setColor(`#81868e`)
+  .setDescription(`c.tempmute used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+  bot.channels.get("575619138576318484").send(used)
 
 
 //end of module
