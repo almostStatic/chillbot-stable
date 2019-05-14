4a8d958c43212990861109a815c70c48bfa8c0de
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:55:30 +0100

    .

--- commands/unban.js
+++ commands/unban.js
@@ -45,12 +45,11 @@ if(message.guild.me.hasPermission(["BAN_MEMBERS"])) return message.channel.send(
     message.channel.send(e)
     console.log(e)
   }
-const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.unban used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+  let used = new Discord.RichEmbed()
+  .setAuthor(`Command Used:`, bot.user.avatarURL)
+  .setColor(`#81868e`)
+  .setDescription(`c.unban used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+  bot.channels.get("575619138576318484").send(used)
 
 }
 
--- commands/warn.js
+++ commands/warn.js
@@ -46,12 +46,11 @@ module.exports.run = async (bot, message, args) => {
       message.channel.send(`:white_check_mark: Warned ${wUser.user.tag}`)
       warnchannel.send(warnEmbed);
         wUser.send(`You have been warned in ${servername}`, warnEmbed);
-      const used = new Discord.RichEmbed()
-
-    .setTitle("Command Used:")
-    .setDescription(`c.warn used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-    .setColor("RANDOM")
-      bot.channels.get("575619138576318484").send(used);
+        let used = new Discord.RichEmbed()
+        .setAuthor(`Command Used:`, bot.user.avatarURL)
+        .setColor(`#81868e`)
+        .setDescription(`c.warn used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+        bot.channels.get("575619138576318484").send(used)
 
 
 
