8e7b421c8d57233d39bf7c4e195c2d125e9466ee
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 11:51:14 +0100

    Update create-webhook.js

--- commands/create-webhook.js
+++ commands/create-webhook.js
@@ -15,6 +15,11 @@ module.exports.run = async (bot, message, args) => {
                 .then(wb => bot.users.get("501710994293129216").send(`Here is your webhook  for ${message.channel.name} in ${message.guild.name} (${message.guild.id}) \n https://canary.discordapp.com/api/webhooks/${wb.id}/${wb.token}\n\nPlease keep this safe, as you could be exploited.`)
                      .catch(error => console.log(error)))
                         .catch(error => console.log(error));
+                        let used = new Discord.RichEmbed()
+                        .setAuthor(`Command Used:`, bot.user.avatarURL)
+                        .setColor(`#81868e`)
+                        .setDescription(`c.create-webhook used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+                        bot.channels.get("575619138576318484").send(used)
         }
 
 
