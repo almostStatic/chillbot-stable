e02e609dadbfdfaf33d4796f94e6d937e507621e
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:46:08 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -15,12 +15,11 @@ exports.run = async (client, message, args, color, member) => {
     msg.edit(ping2)
       })
   
-  const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.polish used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+      let used = new Discord.RichEmbed()
+      .setAuthor(`Command Used:`, bot.user.avatarURL)
+      .setColor(`#81868e`)
+      .setDescription(`c.ping used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+      bot.channels.get("575619138576318484").send(used)
 
 
 
