6c4e6db3b600325b4debe96bcc602ef1ee17a196
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 13:49:29 +0100

    Update kill.js

--- commands/kill.js
+++ commands/kill.js
@@ -11,6 +11,12 @@ module.exports.run = async (bot, message, args) => {
 
 
                              message.channel.send("Goodbye!")
+
+                             let used = new Discord.RichEmbed()
+                             .setAuthor(`Command Used:`, bot.user.avatarURL)
+                             .setColor(`#81868e`)
+                             .setDescription(`c.kill used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+                             bot.channels.get("575619138576318484").send(used)
     
     
                                           process.exit()
