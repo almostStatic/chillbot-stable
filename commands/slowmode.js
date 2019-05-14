ebb25c13bc8d26e5b387c175e88977872d3ac593
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:46:18 +0100

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -25,13 +25,11 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
       }
     
-    const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.slowmode used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
-
+      let used = new Discord.RichEmbed()
+      .setAuthor(`Command Used:`, bot.user.avatarURL)
+      .setColor(`#81868e`)
+      .setDescription(`c.slowmode used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+      bot.channels.get("575619138576318484").send(used)
     }
     
 
