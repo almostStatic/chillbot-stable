0c55ce6d2bb8ea478aaddd58627eb071e33e2a7e
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 13:16:38 +0100

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -39,12 +39,11 @@ if(!owners.includes(message.author.id)) return message.channel.send("This is an 
 
     bot.channels.get("575604330195845149").send(errorEmbed);
     message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-    const used = new Discord.RichEmbed()
-
-    .setTitle("Command Used:")
-    .setDescription(`c.eval used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-    .setColor("RANDOM")
-    bot.channels.get("575619138576318484").send(used);
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.eval used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 
     if(!err) {
       const sucsess = new Discord.RichEmbed()
