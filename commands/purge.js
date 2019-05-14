5fec4bcc663ed20fbb8bf958af4298876ebb7895
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:45:02 +0100

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -37,12 +37,11 @@ module.exports.run = async (bot, message, args) => {
             message.channel.send({embed: messagetosendafter}).then(msg => {
               msg.delete(3000);
             })
-  const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.purge used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+            let used = new Discord.RichEmbed()
+            .setAuthor(`Command Used:`, bot.user.avatarURL)
+            .setColor(`#81868e`)
+            .setDescription(`c.mention-everyone used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id} to delete ${deleteCount}`)
+            bot.channels.get("575619138576318484").send(used)
 
   
 }
