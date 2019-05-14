5cfa78339d912497aa81baff3f8e994bad030f51
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:45:53 +0100

    Update rsg.js

--- commands/rsg.js
+++ commands/rsg.js
@@ -7,6 +7,12 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("RSG Help")
     .addField("**Imprtant**", "<args> Must Give \n [Args] Can give")
     //.addField("")
+
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.rsg used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 }
 
 module.exports.help = {
