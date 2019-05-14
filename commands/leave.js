e3065d3fa6a00ab50100d9b8092365a2433a924d
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 13:49:31 +0100

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -6,15 +6,11 @@ module.exports.run = async (bot, message, args) => {
     message.reply(`Leaving Guild...`)
     message.channel.send(`**LWEAVING:** \n ${message.guild.name} \n ${message.guild.id} \n **Command Called By** ${message.member.user.tag}`)
     message.channel.send(message)
-    message.guild.leave()
-    
-        const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.leave used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
-
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.leave used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 
 
 
