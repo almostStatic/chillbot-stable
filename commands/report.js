50544a638ed5c55548c47fe98ae0471e6c084a5e
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:45:15 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -28,12 +28,11 @@ module.exports.run = async (bot, message, args) => {
     reportschannel.send(reportEmbed);
     message.reply("User has been reported.")
     rUser.send(`You have been reported in ${servername}. Here are the details:`, reportEmbed)
-      const used = new Discord.RichEmbed()
-
-  .setTitle("Command Used:")
-  .setDescription(`c.report used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-  .setColor("RANDOM")
-  bot.channels.get("575619138576318484").send(used);
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.report used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 
 }
  
