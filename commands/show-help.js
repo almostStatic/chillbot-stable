b3b39b9e46cad6d06042a47dbffc9d1b8d2ac93f
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:46:13 +0100

    Update show-help.js

--- commands/show-help.js
+++ commands/show-help.js
@@ -72,13 +72,11 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send({embed: cmds});
         message.channel.send({embed: extras});
     
-    const used = new Discord.RichEmbed()
-
-        .setTitle("Command Used:")
-        .setDescription(`c.show-help used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-        .setColor("RANDOM")
-            bot.channels.get("575619138576318484").send(used);
-
+        let used = new Discord.RichEmbed()
+        .setAuthor(`Command Used:`, bot.user.avatarURL)
+        .setColor(`#81868e`)
+        .setDescription(`c.show-help used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+        bot.channels.get("575619138576318484").send(used)
 
       }
       
