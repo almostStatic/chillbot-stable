06164a7a5785bc86b83c19407c6e61fc7ae5b8e6
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:46:33 +0100

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -6,15 +6,12 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("**sad (Eclipse)#3728** is my owner! He coded every one of my lines!")
     .setColor("RANDOM")
 // 431684183371415555
-    
-            const used = new Discord.RichEmbed()
 
-.setTitle("Command Used:")
-.setDescription(`c.owner used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
-    message.channel.send(ownerembed);
-  
+let used = new Discord.RichEmbed()
+.setAuthor(`Command Used:`, bot.user.avatarURL)
+.setColor(`#81868e`)
+.setDescription(`c.owner used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+bot.channels.get("575619138576318484").send(used)
     
 }
 
