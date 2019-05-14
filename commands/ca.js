00d6ac46b09021e5778cd5d3d8007e2ddb52d1b4
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 11:47:58 +0100

    Update ca.js

--- commands/ca.js
+++ commands/ca.js
@@ -9,6 +9,11 @@ const announcement = new Discord.RichEmbed()
 .setColor("RANDOM")
 
 message.channel.send(announcement);
+let used = new Discord.RichEmbed()
+.setAuthor(`Command Used:`, bot.user.avatarURL)
+.setColor(`#81868e`)
+.setDescription(`c.cc used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+bot.channels.get("575619138576318484").send(used)
 }
 
 module.exports.help = {
