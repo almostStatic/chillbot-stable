1142cfd147f761f001c1fedd62f6ce6607e0aa3d
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 13:45:44 +0100

    Update guildid.js

--- commands/guildid.js
+++ commands/guildid.js
@@ -5,12 +5,12 @@ module.exports.run = async (bot, message, args) => {
     let id = message.guild.id;
 
     message.channel.send(`${id}`);
-        const used = new Discord.RichEmbed()
 
-    .setTitle("Command Used:")
-    .setDescription(`c.guildid used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-    .setColor("RANDOM")
-    bot.channels.get("575619138576318484").send(used);
+    let used = new Discord.RichEmbed()
+    .setAuthor(`Command Used:`, bot.user.avatarURL)
+    .setColor(`#81868e`)
+    .setDescription(`c.guildid used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+    bot.channels.get("575619138576318484").send(used)
 
 
 
