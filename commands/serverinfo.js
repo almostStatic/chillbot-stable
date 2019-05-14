25af662c318e3699ae283a40c935ff8c2db3fc5f
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:47:37 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -16,13 +16,12 @@ let serverembed = new Discord.RichEmbed()
 .setTimestamp()
 
 
-return message.channel.send(serverembed);
-const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.serverinfo used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+ message.channel.send(serverembed);
+ let used = new Discord.RichEmbed()
+ .setAuthor(`Command Used:`, bot.user.avatarURL)
+ .setColor(`#81868e`)
+ .setDescription(`c.serverinfo used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+ bot.channels.get("575619138576318484").send(used)
 
 
 }
