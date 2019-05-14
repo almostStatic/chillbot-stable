9ca04b1942340ccc9d92801da2dea2206f16a2ab
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 14:47:30 +0100

    Update quotes.js

--- commands/quotes.js
+++ commands/quotes.js
@@ -17,13 +17,12 @@ module.exports.run = async (bot, message, args) => {
   .setFooter("Use /invite to invite me to your server!");
   
    message.channel.send(quotes);
-    
-      const used = new Discord.RichEmbed()
 
-.setTitle("Command Used:")
-.setDescription(`c.quotes used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+   let used = new Discord.RichEmbed()
+   .setAuthor(`Command Used:`, bot.user.avatarURL)
+   .setColor(`#81868e`)
+   .setDescription(`c.quotes used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+   bot.channels.get("575619138576318484").send(used)
 
   
 }
