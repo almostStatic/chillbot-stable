8150eb98dad35444f3e5d751c447491eabf6fa03
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 11:48:02 +0100

    Update botconfig.js

--- commands/botconfig.js
+++ commands/botconfig.js
@@ -12,12 +12,12 @@ const embed = new Discord.RichEmbed()
     .setFooter("a footer", message.author.avatarURL)
 
         message.channel.send(embed);
-        const used = new Discord.RichEmbed()
-
-    .setTitle("Command Used:")
-    .setDescription(`c.botconfig used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-    .setColor("RANDOM")
-    bot.channels.get("575619138576318484").send(used);
+        let used = new Discord.RichEmbed()
+        .setAuthor(`Command Used:`, bot.user.avatarURL)
+        .setColor(`#81868e`)
+        .setDescription(`c.botconfig used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+        bot.channels.get("575619138576318484").send(used)
+      //  const used = new Discord.RichEmbed()
 
 
 
