22eaddaf780931bbbffb487d6f060d8c9f49c69e
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 11:34:01 +0100

    fixed ${message.author.ud to ${message.author.id}

--- commands/actoins.js
+++ commands/actoins.js
@@ -38,7 +38,7 @@ module.exports.run = async (bot, message, args) => {
     let used = new Discord.RichEmbed()
     .setAuthor(`Command Used:`, bot.user.avatarURL)
     .setColor(`#81868e`)
-    .setDescription(`c.actions used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.ud}`)
+    .setDescription(`c.actions used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
     bot.channels.get("575619138576318484").send(used)
 }
 
