38607d05519fe435ec8c01b035fa441ca8bb0bc4
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-14 13:54:49 +0100

    Update mention-everyone.js

--- commands/mention-everyone.js
+++ commands/mention-everyone.js
@@ -10,14 +10,13 @@ module.exports.run = async (bot, message, args) => {
     
     
         if(!message.member.hasPermission('MENTION_EVERYONE')) return message.channel.send({embed: everyone});
-        return message.channel.send(`<@${message.author.id}> mentioned @everyone!`)
+         message.channel.send(`<@${message.author.id}> mentioned @everyone!`)
   
-      const used = new Discord.RichEmbed()
-
-.setTitle("Command Used:")
-.setDescription(`c.mention-everyone used in ${message.guild.name} (${message.guild.id}), by ${message.author}, (${message.author.id})`)
-.setColor("RANDOM")
-bot.channels.get("575619138576318484").send(used);
+        let used = new Discord.RichEmbed()
+        .setAuthor(`Command Used:`, bot.user.avatarURL)
+        .setColor(`#81868e`)
+        .setDescription(`c.mention-everyone used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+        bot.channels.get("575619138576318484").send(used)
       
     
 
