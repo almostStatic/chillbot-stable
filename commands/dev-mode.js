610cc6550f8b75129c48077a35c1a62aeb7f13d3
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-05-28 13:27:38 +0100

    added snippet

--- commands/dev-mode.js
+++ commands/dev-mode.js
@@ -1,9 +1,6 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
-    
-
     const devembed = new Discord.RichEmbed()
 
     .setTitle("How to Enable Developer Mode")
@@ -13,7 +10,6 @@ module.exports.run = async (bot, message, args) => {
     .addField("How to enable Dev Mode on **Android**?", "On Android, you can enable Developer Mode by going to your Discord settings (the ⚙️️️ next to your name at the bottom left) and tap on Behavior. There you will find Developer Mode. Tap the toggle to enable it.")
     .addField("How to enable Dev Mode on **IOS**?", "If you have iOS, you can enable Developer Mode by opening your Discord settings  in the bottom-right corner of the screen, tapping on Appearance, and flicking the switch labeled Developer Mode.")
     .addField("**Usage:**", "Developer Mode adds a simple Copy ID option to your context menu. Right click or long press on any server, user, message or channel to get its unique ID.")
-    .setFooter("Use /invite to invite me to your server!")
 
     message.channel.send({embed: devembed});
     let used = new Discord.RichEmbed()
@@ -21,9 +17,6 @@ module.exports.run = async (bot, message, args) => {
     .setColor(`#81868e`)
     .setDescription(`c.dev-mode used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
     bot.channels.get("575619138576318484").send(used)
-
-
-
   }
 
 module.exports.help = {
