3b5166013792e83c8b2eba270c281a505f72cbce
sad (Eclipse)#3728 <49240408+[emil.private]@users.noreply.github.com>
2019-06-03 13:57:05 +0100

    added fixmute.all (not listed on help cmd)

---
+++ commands/fixmute.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(bot, message, args)=>{
+    if(!message.member.hasPermission('MANAGE_GUILD')){
+        message.delete(50);
+        message.reply('Only admins can use this command to fix mute')
+    }
+    message.guild.channels.forEach((channels) => {
+        channels.overwritePermissions('584164297655975983', {
+            'VIEW_CHANNEL': true,
+            'SEND_MESSAGES': false, 
+            'READ_MESSAGE_HISTORY': true
+        })
+    });
+
+    await message.reply('Sucsessfully overwritten channel permissoins for the `muted` role to not be able to send messages!')
+}
+
+module.exports.help = {
+    name: 'fixmute.all'
+}
