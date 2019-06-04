4c5d2f07a9d87118a8588e9f66b21c738bd09db4
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-06-04 22:52:50 +0100

    Create permcheck.js

---
+++ commands/permcheck.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+
+module.exports.run = async(bot, message, args) => {
+if(message.channel){
+    return;
+}
+
+let used = new Discord.RichEmbed()
+.setAuthor(`Command Used:`, bot.user.avatarURL)
+.setColor(`#81868e`)
+.setDescription(`c.perms used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
+bot.channels.get("575619138576318484").send(used)
+
+    }
+
+module.exports.help = {
+    name: 'perms'
+}
