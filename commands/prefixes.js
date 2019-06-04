3837fbaa6a691f0219fd7d17b77b2a61820353ae
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-06-04 22:30:28 +0100

    Create prefixes.js

---
+++ commands/prefixes.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json")
+var prefix = botconfig.prefixes;
+module.exports.run = async(bot, message, args) => {
+    let parsed = JSON.parse(prefix)
+    let prefixesEmbed = new Discord.RichEmbed()
+    .setTitle("Bot Prefixes")
+    .addField("Prefixes:", 'c.\nbot.run>>\n/\n')
+
+    message.channel.send({embed: prefixesEmbed});
+
+}
+
+module.exports.help = {
+    name: 'prefixes'
+}
