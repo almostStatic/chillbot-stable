0e1743c19839b4a87510d70c6835cafb7e4bbb42
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-06-04 13:09:17 +0100

    Update roleinfo.js

--- commands/roleinfo.js
+++ commands/roleinfo.js
@@ -1,8 +1,5 @@
 const Discord = require("discord.js");
 
-
-
-
 module.exports.run = async (bot, message, args) => {
 
   let norole = new Discord.RichEmbed()
@@ -10,16 +7,18 @@ module.exports.run = async (bot, message, args) => {
   .setColor("#ff0000")
 
   if(message.channel.type === "dm") return message.reply("❌ You may not use this command in a DM channel");
-  let roletocheck = args.join(" ")
+  let roletocheck = args[0]
   let role = bot.guilds.get(message.guild.id).roles.find('name', roletocheck);
-  if (!role) return message.channel.send(norole)
+  if (!role) return message.channel.send('That role does not exist')
     const embed = new Discord.RichEmbed()
 
     .setColor("RANDOM")
+    .setTitle(`Role Information | ${args[0]}`)
     .addField('Role name', `${role.name}`, true)
     .addField('Role ID', `${role.id}`, true)
-    .addField('Created At', role.createdAt.toDateString())
-    .addField("Mentionable: ", role.mentionable ? 'Yes' : 'No')
+    .addField('Created At', role.createdAt.toDateString(), true)
+    .addField("Mentionable: ", role.mentionable ? 'Yes' : 'No', true)
+    .addField('Key Permissions', role.permissions, true)
 
     message.channel.send(embed);
   
