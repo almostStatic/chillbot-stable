acb4f9826af449fef55df02bd544e8aaa3198249
sad (Eclipse)#3728 <49240408+asadhum2005@users.noreply.github.com>
2019-06-04 13:09:07 +0100

    Update fixmute.js

--- commands/fixmute.js
+++ commands/fixmute.js
@@ -3,17 +3,21 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args)=>{
     if(!message.member.hasPermission('MANAGE_GUILD')){
         message.delete(50);
-        message.reply('Only admins can use this command to fix mute')
+        message.reply('Only staff can use this command to fix mute')
     }
     message.guild.channels.forEach((channels) => {
         channels.overwritePermissions('584164297655975983', {
             'VIEW_CHANNEL': true,
-            'SEND_MESSAGES': false, 
-            'READ_MESSAGE_HISTORY': true
+            'SEND_MESSAGES': false,
+            'READ_MESSAGE_HISTORY': true,
+            'ADD_REACTIONS': false
         });
     });
-
-    await message.reply('Sucsessfully overwritten channel permissoins for the `muted` role to not be able to send messages!')
+    message.reply(`Overwriting channel permissions... Please Wait...`).then((msg)=> {
+        setTimeout(function(){
+            msg.edit('Sucsessfully changed permissions for channes! The mute role may no longer sned messages, or react to them!')
+        }, 3000)
+      }); 
 }
 
 module.exports.help = {
