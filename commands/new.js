21094962a2fa24cddaf258240d2f097ecdabccc4
sad (Eclipse)#3728 <49240408+[emil.private]@users.noreply.github.com>
2019-06-06 10:23:33 +0100

    Update new.js

--- commands/new.js
+++ commands/new.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+
 module.exports.run = async (bot, message, args) => {
     
     let reason = args.join(" ")
@@ -8,7 +9,28 @@ module.exports.run = async (bot, message, args) => {
     everyone = "507889693816520724";
     let embed = new Discord.RichEmbed()
     .setTitle(`Your Inquiry: ${reason}`)
-    message.guild.createChannel(`${message.member.user.tag}`, { parent: "582255605058764801"}).then(ch => ch.setTopic(reason)).then((channel)=> {
+
+    message.guild.createChannel(`${message.member.user.tag}`, { parent: "582255605058764801"}, {
+      permissionOverwrites: [{
+        id: message.guild.id, 
+        deny: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+        allow: ['SEND_MESSAGES']
+      },
+      {
+        id: message.author.id,
+        allow: ['MANAGE_CHANNEL', 'VIEW_CHANNEL', 'SEND_MESSAGES'],
+        deny: ['MANAGE_MESSAGES']
+      },
+      {
+        id: '580683463976419344',
+        allow: ['MANAGE_CHANNEL', 'MANAGE_MESSAGES']
+      }
+    ]
+    });
+
+    message.channel.send('Your ticket has been created!')
+
+    /*).then(ch => ch.setTopic(reason)).then((channel)=> {
         setTimeout(function(){
             // Actions
            channel.overwritePermissions(everyone, {
@@ -22,12 +44,12 @@ module.exports.run = async (bot, message, args) => {
           });
           
             channel.send(`Hello ${message.author}! Thank you for contacting our mod team! \n We will respond to you here as soon as possible. \n Only you and the staff team have access to **this** channel!`, embed)
-            channel.send(`<@}} PING REMOVED { 580683463976419344>, new case **#${message.id}** \n I have granted staff members (and ${message.author}) permissions to remove this channel once used. ${message.member.user.tag}, you may delete this channel if you wish. `)
-            message.channel.send("Resolving...").then(a => a.edot(` <:GreenTick:580716592980164618> ${message.author} Your ticket has been created <#${channel.id}>`));
+            channel.send(`<@&580683463976419344>, new case **#${message.id}** \n I have granted staff members (and ${message.author}) permissions to remove this channel once used. ${message.member.user.tag}, you may delete this channel if you wish. `)
+            message.channel.send("Resolving...").then(a => a.edit(` <:GreenTick:580716592980164618> ${message.author} Your ticket has been created <#${channel.id}>`));
 
         }, 3000)
-
-      }); 
+*/
+//      }); 
 
     
     // <@&580683463976419344>
