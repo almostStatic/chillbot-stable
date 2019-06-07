32fc1f8a4fa3af52a4977263997d940bc155ab65
sad (Eclipse)#3728 <49240408+[emil.private]@users.noreply.github.com>
2019-06-07 12:13:59 +0100

    add updates.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,30 +2,27 @@ const Discord = require("discord.js");
 const snekfetch = require('snekfetch')
 const fs = require("fs")
 
-exports.run = async (client, message, args, color, member) => {
-  let ping = Math.round(message.client.ping); 
+exports.run = async (bot, message, args) => {
+  let ping = Math.round(message.bot.ping); 
   const ping1 = new Discord.RichEmbed()
   .setDescription(`:ping_pong: Please wait! It wont take long :) \n if you see this message its probs not a good thing`)
   .setColor("RANDOM");
   message.channel.send({embed: ping1}).then((msg) => {
     const ping2 = new Discord.RichEmbed()
-    .addField('__**API:**__', `${ping}`, true)
-    .addField('__**Ping:**__', `${msg.createdTimestamp - message.createdTimestamp} ms`, true)
+    .addField('__**API:**__', `${ping} MS`, true)
+    .addField('__**Ping:**__', `${msg.createdTimestamp - message.createdTimestamp} MS`, true)
     .setColor('RANDOM');
     msg.edit(ping2)
-      })
+      });
   
       let used = new Discord.RichEmbed()
       .setAuthor(`Command Used:`, bot.user.avatarURL)
       .setColor(`#81868e`)
       .setDescription(`c.ping used in ${message.guild.name} (${message.guild.id}) \n ${message.author.username}#${message.author.discriminator}, ${message.author.id}`)
       bot.channels.get("575619138576318484").send(used)
-
-
-
 };
 
 
 module.exports.help = {
-    name: "ping"
-  }
+    name: "ping",
+  };
---
+++ commands/updates.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+
+module.exports.run = async(bot, message, args)=>{
+    const embed = new Discord.RichEmbed()
+    .setColor(0x7aa7ef)
+    .setThumbnail(bot.user.avatarURL)
+    .setTitle("Updates ")
+    .addField("New Features", "Multiple bot prefixes `c.`, `>`\nAnother user has received eval permission\nWelcome Messages are here!\nLeave Messages are also here!")
+    .addField("New Commands", "Add `c.updates` to view bot updates\nc.docs [inquiry] - search the **discord.js** documentation\n**(Staff Only)** c.partner [server rep] [invite_url] [server description]\nc.files see how many command files are in use and are usable, and how much memory they are taking up\nUse c.uptime to see the bot's current uptime\n**(Staff Only) `c.fixmute.all [fix mute for the server]`**\n`c.emojis` get a list of server emojis!")
+    .addField("Updated Commands", 'c.membercount - Updated to show bots and humans seperately\nc.botinfo, displays memory used and version of discord.js AND current number of voice connections\nc.serverinfo redone, and shows server region')
+    .addField("Fixed Commands", 'Fixed c.suggest')
+    .addField("Guild Changes", ' :flag_gb: Server region is set to London')
+
+    message.channel.send({ embed });
+};
+
+
+module.exports.help = {
+    name: 'updates',
+};
