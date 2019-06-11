067468b8b8d6a8f2ce720e99690dcbb12848b685
sad (Eclipse)#3728 <49240408+[emil.private]@users.noreply.github.com>
2019-06-11 13:24:41 +0100

    Update listroles.js

--- commands/listroles.js
+++ commands/listroles.js
@@ -2,7 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) =>{
     const rolelist = message.guild.roles.map(r=>r.name.toString()).join("\n");
-    message.channel.send(`**__Role List__**\n\`\`\`\n${rolelist}\n\`\`\``)
+    let embed = new Discord.RichEmbed()
+    .setTitle('Roles')
+    .setDescription(`\`\`\`css\n${rolelist}\n\`\`\``)
+        message.channel.send({ embed })
+    //  message.channel.send(`**__Role List__**\n\`\`\`\n${rolelist}\n\`\`\``)
 }
 
 module.exports.help = {
