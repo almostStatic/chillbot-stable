824feeb6aa6f16cbe5c8cabb7b5a43e195984c09
sad (Eclipse)#3728 <49240408+[emil.private]@users.noreply.github.com>
2019-07-09 10:38:19 +0100

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -1,34 +1,35 @@
 const Discord = require("discord.js");
-
+const moment = require('moment');
 module.exports.run = async (bot, message, args) => {
 
 
   if(message.channel.type === "dm") return message.reply("❌ You may not use this command in a DM channel");
     let rUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let rreason = args.join(" ").slice(22);
+    let reportedAt = `${moment().format("YYYY-MM-DD HH:mm:ss")}`
     if(!rUser) return message.channel.send("Error 404: user not found... :(");
     let servername = message.guild.name;
     let reportEmbed = new Discord.RichEmbed()
     
     .setTitle(`Action: Report -> ${rUser.user.tag}`)
     .setColor("#ff0000")
-    .addField("Reported User", `${rUser}, (${rUser.id})`, true)
-    .addField("Reported By", `${message.author}, (${message.author.id})`, true)
-    .addField("Channel", message.channel, true)
-    .addField("Time", message.createdAt.toDateString(), true)
-    .addField("Reason", rreason)
+    .addField("> Reported User", `${rUser}, (${rUser.id})`, true)
+    .addField("> Reported By", `${message.author}, (${message.author.id})`, true)
+    .addField("> Reported In", message.channel, true)
+    .addField("> Reported At", reportedAt, true)
+    .addField("> Reason", rreason)
     .setThumbnail(rUser.user.avatarURL)
     .setTimestamp()
+    .setFooter(`> ID: ${rUser.user.id}`, message.author.avatarURL);
 
-    let reportschannel = message.guild.channels.find(`name`, "reports");
-
-    if(!reportschannel) return message.channel.send(`❌ **Couldn't find reports channel. Report has been send in <#${message.channel.id}>**`, reportEmbed);
+    let reportschannel = message.guild.channels.find(c=>c.name==='reports');
+    if(!reportschannel) return message.channel.send(`❌ **Couldn't** find reports channel. Please contact an admin!`);
 
 
     message.delete().catch(O_o=>{});
     reportschannel.send(reportEmbed);
-    message.reply("User has been reported.")
-    rUser.send(`You have been reported in ${servername}. Here are the details:`, reportEmbed)
+    message.reply(`<:GreenTick:580716592980164618> **${rUser.user.tag}** has been reported!`)
+    rUser.send(`You have been reported in **${servername}**! Here are the details:`, reportEmbed)
     let used = new Discord.RichEmbed()
     .setAuthor(`Command Used:`, bot.user.avatarURL)
     .setColor(`#81868e`)
