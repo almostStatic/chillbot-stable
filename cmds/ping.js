0143c799ea721d06edb0e4e1d2d425e2dce45962
sad (Eclipse)#3728 <asadhum2005@gmail.com>
2019-11-26 14:51:28 +0000

    added ping command and updated command handler

---
+++ cmds/ping.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+exports.run = async (client, message, args) => {
+  let ping = Math.round(message.client.ping); 
+  const ping1 = new Discord.RichEmbed()
+  .setDescription(`:ping_pong: Please wait! It wont take long :) \n if you see this message its probs not a good thing`)
+  .setColor("RANDOM");
+  message.channel.send({embed: ping1}).then((msg) => {
+  const ping2 = new Discord.RichEmbed()
+  .addField('__**API:**__', `${ping}`, true)
+  .addField('__**Ping:**__', `${msg.createdTimestamp - message.createdTimestamp} ms`, true)
+  .setColor('RANDOM');
+  msg.edit(ping2)
+    });
+};
+
+module.exports.help = {
+	name: 'ping',
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const Discord = require("discord.js")
+const client = new Discord.Client({ disableEveryone: true })
+
+client.commands = new Discord.Collection();
+
+fs.readdir("./cmds/", (err, cmds) => {
+
+  if (err) { 
+		console.log(err);
+	}
+  let jsfile = cmds.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Commands Not Found");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./cmds/${f}`);
+    console.log(`found ${jsfile.length} commands to laod`);
+    client.commands.set(props.help.name, props);
+  });
+});
+
+client.on("ready", async() => { 
+	console.log(`${client.user.username} is now online!`)
+	console.log(client.guilds.map(g=>g.toString()).join("\n"))
+});
+
+client.on("message", async(message) => {
+	if (message.author.bot) return;
+  if (message.channel.type === "dm") {
+		client.channels.get('600639235938320399').send(`**${message.author.tag}**: ${message.content}`, {
+			embed: new Discord.RichEmbed()
+			.setFooter("ID: " + message.author.id)
+			.setTimestamp()
+			
+		});
+	};
+
+  let prefix = process.env.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLowerCase();
+  let args = messageArray.slice(1);
+  let commandfile = client.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(client,message,args);
+})
+
+client.login(process.env.token)
