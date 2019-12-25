a9e8a8f8552469434c1711bef461398fc3f8e188
Eclipse.js <asadhum2005@gmail.com>
2019-12-25 21:19:16 +0000

    the bans command is FINALLY HERE ! After 2 hours of pain, I have finally been able to make this command work! However, it does not show the reason yet. I have yet to  add that to this command, however that is extremely complicated and so i cannot do so now. Be sure that is on my future endeavours!!!!!

--- cmds/bans.js
+++ cmds/bans.js
@@ -1,13 +1,23 @@
 const Discord = require("discord.js")
 
 module.exports.run = async(client, message, args) => {
-	await message.guild.fetchBans()
-		.then(bans => {
-			message.channel.send(bans.size)
+	if (!message.member.permissions.has("BAN_MEMBERS")) {
+		return message.channel.send(`${process.env.re} You do not have correct permissinos to use this command.\n> You need the **ban members** permission!`)
+	}
+	 message.guild.fetchBans(true)
+		.then(async(bans) => {
+			let bList = bans.map(b => `${b.user.tag} | ${b.user.id}`.toString()).join("\n")
+			message.channel.send(`
+Users banned from **${message.guild.name}**:
+\`\`\`
+${bList}
+\`\`\`
+`, {
+	split: true
+})
 		})
 			.catch(er => {
-				message.channel.send(er, { code: "xl", split: true })
-				//message.reply(`${process.env.re} | I was unable to find your bans list.\n> Check my permissions`)
+				message.reply(`${process.env.re} | I was unable to find your bans list.\n> Check my permissions`)
 			})
 };
 
