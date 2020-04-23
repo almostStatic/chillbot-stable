f170f502c87a1e34dcd87d3489bb3929a8c39fe1
Static <asadhum2005@gmail.com>
2020-04-23 14:48:46 +0000

    instanceof Promise added to eval

--- cmds/eval.js
+++ cmds/eval.js
@@ -1,17 +1,6 @@
 let Discord = require("discord.js");
 let json = require("async-jsonstore-io");
 let keyv = require('keyv');
-let jsonstore = new json(process.env.jstk);
-let editsnipes = new keyv('sqlite://./database/editsnipes.sqlite');
-let wtf = new keyv('sqlite://./database/wtf.sqlite');
-let botperms = new keyv("sqlite://./database/botperms.sqlite");
-let welcomes = new keyv("sqlite://./database/cwelcome.sqlite");
-let cmdCount = new keyv("sqlite://./database/cmdCount.sqlite");
-let hides = new keyv('sqlite://./database/hides.sqlite')
-let snipes = new keyv("sqlite://./database/snipes.sqlite");
-let prefixes = new keyv("sqlite://./database/prefixes.sqlite");
-let mutes = new keyv('sqlite://./database/mutes.sqlite');
-let blacklisted = new keyv("sqlite://./database/blacklisted.sqlite");
 let logs = new keyv("sqlite://./database/log.sqlite");
 let colors = new keyv("sqlite://./database/colors.sqlite");
 
@@ -44,31 +33,35 @@ async run(client,message,args,prefix,jsonColor,sleep,done,error) {
 			return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
 		}
 		let code = args.join(" ")
+		if (!code) {
+			return msg.edit(process.env.re + " You need to give me some code to evaluate!")
+		};
 		code = code.toString().replace(new RegExp(escapeRegExp(client.token), 'g'), '<token removed>');
 		
 		if(code.includes('forEach')) return msg.edit(process.env.re + ' forEach is forbidden!')
 		if(code.includes('token')) return msg.edit(process.env.re + ' client token cannot be evaled!')
 		if(code.includes('client[')) return msg.edit(process.env.re + ' client[ cannot be evaled!')
 		if(code.includes('client [')) return msg.edit( process.env.re + ' client [ cannot be evaled!')
-		let evaled = await eval(code);
-		if (!code) {
-			return msg.edit("You need to give me some code to evaluate!")
-		};
+		let evaled;
+			if (code instanceof Promise) {
+				evaled = await eval(code);
+			} else {
+				evaled = eval(code);
+			};
 		if (typeof evaled !== "string")
 			evaled = await require("util")
 				.inspect(evaled);
 			let cleaned = await clean(evaled);
 			cleaned = client.trim(cleaned, 2030)
 			cleaned = await cleaned.replace(client.token, '[token hidden]')
-			console.log(cleaned.length)
-		await msg.edit("", {
+		 msg.edit("", {
 			embed: new Discord.RichEmbed()
 			.setTitle("Evaluation Successful!")
 			.setDescription(`\`\`\`xl\n${cleaned}\n\`\`\``)
 			.setColor(jsonColor)
 		});
 	} catch (err) {
-		await	msg.edit("", {
+			msg.edit("", {
 				embed: new Discord.RichEmbed()
 				.setTitle("Evaluation Unsuccessful")
 				.setDescription(`\`\`\`xl\n${err}\n\`\`\``)
